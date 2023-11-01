4b82b34b5affaf1d9a8ffd342d896bca13b9db60
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-11-01 16:21:43 +0200

    add gitignore

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-const { listContacts, getContactById } = require("./contacts");
+const contacts = require("./contacts");
+const argv = require("yargs").argv;
 
-//listContacts()
-//  .then((data) => console.log(data))
-//  .catch((err) => console.error(err));
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      contacts
+        .listContacts()
+        .then((data) => console.table(data))
+        .catch((err) => console.error(err));
+      break;
 
-getContactById("AeHIrLTr6JkxGE6SN-0Rw")
-  .then((data) => console.log(data))
-  .catch((err) => console.error(err));
+    case "get":
+      contacts
+        .getContactById("AeHIrLTr6JkxGE6SN-0Rw")
+        .then((data) => console.log(data))
+        .catch((err) => console.error(err));
+      break;
+
+    case "add":
+      // ... name email phone
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
